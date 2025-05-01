784203f19396fd53bc550018312ce8a9c58c2335
andreamancuso <and.mancuso@googlemail.com>
2025-05-01 12:20:28 +0200

    Now collecting almost everything needed to generate .env and docker-compose.yml

--- cli/cmd/quickStart.go
+++ cli/cmd/quickStart.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/url"
 	"os"
 	"os/exec"
 	"strings"
@@ -54,6 +55,39 @@ func FlattenStacks(stacks []StackDescriptor) []FlattenedTag {
 	return flat
 }
 
+type QuickStartConfig struct {
+	Arch                     string
+	SelectedTags             []string
+	UseContainerizedPostgres bool
+	UseGrafana               bool
+
+	RawPair struct {
+		Host     string
+		Protocol string
+		Port     string
+		BasePath string
+	}
+
+	TerminalService struct {
+		Host string
+		Port string
+	}
+
+	Grafana struct {
+		Host string
+		Port string
+	}
+
+	Db struct {
+		Host     string
+		Port     string
+		Name     string
+		User     string
+		Password string
+		Url      string
+	}
+}
+
 func fetchRawPairStacks() ([]FlattenedTag, error) {
 	url := "https://raw.githubusercontent.com/rawpair/stacks/refs/heads/main/stacks/stacks.json"
 
@@ -83,15 +117,21 @@ var quickStartCmd = &cobra.Command{
 	Use:   "quickStart",
 	Short: "Interactively choose arch and stacks to build",
 	Run: func(cmd *cobra.Command, args []string) {
-		// Detect architecture
+		cfg := QuickStartConfig{}
+
 		defaultArch := runtime.GOARCH
-		var arch string
+
+		var selectedTagOptions []string
+
+		labelMap := make(map[string]FlattenedTag)
+		var displayOptions []string
+
 		promptArch := &survey.Select{
 			Message: "Choose your architecture:",
 			Options: []string{"amd64", "arm64"},
 			Default: defaultArch,
 		}
-		err := survey.AskOne(promptArch, &arch)
+		var err = survey.AskOne(promptArch, &cfg.Arch)
 		if err != nil {
 			fmt.Println("Aborted or failed:", err)
 			return
@@ -103,12 +143,10 @@ var quickStartCmd = &cobra.Command{
 			return
 		}
 
-		labelMap := make(map[string]FlattenedTag)
-		var displayOptions []string
 		for _, tag := range flattenedTags {
 			var supportsArch bool
 			for _, platform := range tag.Platforms {
-				if strings.HasSuffix(platform, arch) {
+				if strings.HasSuffix(platform, cfg.Arch) {
 					supportsArch = true
 					break
 				}
@@ -122,10 +160,8 @@ var quickStartCmd = &cobra.Command{
 			labelMap[label] = tag
 		}
 
-		var selectedTagOptions []string
-		var selectedTags []FlattenedTag
 		promptStacks := &survey.MultiSelect{
-			Message:  "Select stacks to build:",
+			Message:  "Select stacks to include (will be built locally through `docker pull`):",
 			Options:  displayOptions,
 			PageSize: 8,
 		}
@@ -135,12 +171,18 @@ var quickStartCmd = &cobra.Command{
 		}
 
 		fmt.Println("\nSelected options:")
-		fmt.Println("Architecture:", arch)
+		fmt.Println("Architecture:", cfg.Arch)
 		fmt.Println("Stacks:", selectedTagOptions)
 
-		for _, name := range selectedTagOptions {
-			fmt.Printf("📦 Pulling rawpair/%s:latest...\n", name)
-			cmd := exec.Command("docker", "pull", fmt.Sprintf("rawpair/%s:latest", name))
+		for _, label := range selectedTagOptions {
+			if tag, ok := labelMap[label]; ok {
+				cfg.SelectedTags = append(cfg.SelectedTags, tag.Id)
+			}
+		}
+
+		for _, name := range cfg.SelectedTags {
+			fmt.Printf("Pulling rawpair/%s...\n", name)
+			cmd := exec.Command("docker", "pull", fmt.Sprintf("rawpair/%s", name))
 			cmd.Stdout = os.Stdout
 			cmd.Stderr = os.Stderr
 			if err := cmd.Run(); err != nil {
@@ -148,9 +190,172 @@ var quickStartCmd = &cobra.Command{
 			}
 		}
 
-		for _, label := range selectedTagOptions {
-			if tag, ok := labelMap[label]; ok {
-				selectedTags = append(selectedTags, tag)
+		err = survey.AskOne(&survey.Confirm{
+			Message: "Use containerized Postgres? (Not recommended for production)",
+			Default: false,
+		}, &cfg.UseContainerizedPostgres)
+
+		if err != nil {
+			fmt.Println("Aborted or failed:", err)
+			return
+		}
+
+		if !cfg.UseContainerizedPostgres {
+			fmt.Println("Using non-containerized Postgres")
+			fmt.Println("Please ensure that Postgres is running and accessible.")
+
+			promptDbHost := &survey.Input{
+				Message: "DB Host:",
+				Default: "localhost",
+			}
+			err = survey.AskOne(promptDbHost, &cfg.Db.Host)
+			if err != nil {
+				fmt.Println("Aborted or failed:", err)
+				return
+			}
+
+			promptDbPort := &survey.Input{
+				Message: "DB Port:",
+				Default: "5432",
+			}
+			err = survey.AskOne(promptDbPort, &cfg.Db.Port)
+			if err != nil {
+				fmt.Println("Aborted or failed:", err)
+				return
+			}
+
+			promptDbUser := &survey.Input{
+				Message: "DB User:",
+				Default: "postgres",
+			}
+			err = survey.AskOne(promptDbUser, &cfg.Db.User)
+			if err != nil {
+				fmt.Println("Aborted or failed:", err)
+				return
+			}
+
+			promptDbPassword := &survey.Input{
+				Message: "DB Password:",
+				Default: "postgres",
+			}
+			err = survey.AskOne(promptDbPassword, &cfg.Db.Password)
+			if err != nil {
+				fmt.Println("Aborted or failed:", err)
+				return
+			}
+
+			promptDbName := &survey.Input{
+				Message: "DB Name:",
+				Default: "rawpair",
+			}
+			err = survey.AskOne(promptDbName, &cfg.Db.Name)
+			if err != nil {
+				fmt.Println("Aborted or failed:", err)
+				return
+			}
+
+			cfg.Db.Url = fmt.Sprintf("postgres://%s:%s@%s:%s/%s", url.QueryEscape(cfg.Db.User), url.QueryEscape(cfg.Db.Password), cfg.Db.Host, cfg.Db.Port, cfg.Db.Name)
+
+			fmt.Println("Database URL:", cfg.Db.Url)
+
+		}
+
+		if err != nil {
+			fmt.Println("Aborted or failed:", err)
+			return
+		}
+
+		promptRawPairProtocol := &survey.Select{
+			Message: "RawPair Protocol:",
+			Options: []string{"http", "https"},
+			Default: "http",
+		}
+
+		err = survey.AskOne(promptRawPairProtocol, &cfg.RawPair.Protocol)
+		if err != nil {
+			fmt.Println("Aborted or failed:", err)
+			return
+		}
+
+		promptRawPairHost := &survey.Input{
+			Message: "RawPair Host:",
+			Default: "localhost",
+		}
+		err = survey.AskOne(promptRawPairHost, &cfg.RawPair.Host)
+		if err != nil {
+			fmt.Println("Aborted or failed:", err)
+			return
+		}
+
+		promptRawPairPort := &survey.Input{
+			Message: "RawPair Port:",
+			Default: "4000",
+		}
+		err = survey.AskOne(promptRawPairPort, &cfg.RawPair.Port)
+		if err != nil {
+			fmt.Println("Aborted or failed:", err)
+			return
+		}
+
+		promptRawPairBasePath := &survey.Input{
+			Message: "RawPair Base Path:",
+			Default: "/",
+		}
+		err = survey.AskOne(promptRawPairBasePath, &cfg.RawPair.BasePath)
+		if err != nil {
+			fmt.Println("Aborted or failed:", err)
+			return
+		}
+
+		promptTerminalServiceHost := &survey.Input{
+			Message: "Terminal Service Host:",
+			Default: "localhost",
+		}
+		err = survey.AskOne(promptTerminalServiceHost, &cfg.TerminalService.Host)
+		if err != nil {
+			fmt.Println("Aborted or failed:", err)
+			return
+		}
+
+		promptTerminalServicePort := &survey.Input{
+			Message: "Terminal Service Port:",
+			Default: "8080",
+		}
+		err = survey.AskOne(promptTerminalServicePort, &cfg.TerminalService.Port)
+		if err != nil {
+			fmt.Println("Aborted or failed:", err)
+			return
+		}
+
+		err = survey.AskOne(&survey.Confirm{
+			Message: "Use Grafana?",
+			Default: false,
+		}, &cfg.UseGrafana)
+
+		if err != nil {
+			fmt.Println("Aborted or failed:", err)
+			return
+		}
+
+		if cfg.UseGrafana {
+			promptGrafanaHost := &survey.Input{
+				Message: "Grafana Host:",
+				Default: "localhost",
+			}
+			err = survey.AskOne(promptGrafanaHost, &cfg.Grafana.Host)
+			if err != nil {
+				fmt.Println("Aborted or failed:", err)
+				return
+			}
+
+			promptGrafanaPort := &survey.Input{
+				Message: "Grafana Port:",
+				Default: "3000",
+			}
+			err = survey.AskOne(promptGrafanaPort, &cfg.Grafana.Port)
+			if err != nil {
+				fmt.Println("Aborted or failed:", err)
+				return
 			}
 		}
 	},
